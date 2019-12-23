564908209213ce3084f3667eaf02880e5221444c
jpca999 <jpca999@gmail.com>
2019-12-22 19:54:28 -0500

    post call to the backend working nice

--- src/models/fieldlist.model.js
+++ src/models/fieldlist.model.js
@@ -7,15 +7,16 @@ module.exports = function (app) {
   const { Schema } = mongooseClient;
   const fieldlist = new Schema({
     clientId: { type: String, required: false },
-    header: { type: String, required: false },
-    fieldName: { type: String, required: false },
-    mappedFieldName: { type: String, required: false },
-    maxcharLength: { type: String, required: false },
-    sequenceNumber: { type: String, required: false },
+    HeaderInfo: { type: String, required: false },
+    FieldName: { type: String, required: false },
+    MappedFieldName: { type: String, required: false },
+    MaxCharLength: { type: String, required: false },
+    SequenceNumber: { type: String, required: false },
     alignment: { type: String, required: false },
-    paddingLeft: { type: String, required: false },
+    PaddingLeft: { type: String, required: false },
     paddingLeftLength: { type: String, required: false },
-    paddingRight: { type: String, required: false },
+    PaddingRight: { type: String, required: false },
+    paddingRightLength: { type: String, required: false },    
   }, {
     timestamps: true
   });
